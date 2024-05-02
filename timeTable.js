947fc25ffafdb4480732eff16e1e69db35667b17
Lahu S. Andhale <111334418+Lahu19@users.noreply.github.com>
2024-05-02 15:49:22 +0530

    I did changes

---
+++ timeTable.js
@@ -0,0 +1,134 @@
+const Sunday =[
+    {   
+        time: 'Sunday',
+        roomNumber: 'Holiday',
+        subject: 'No class Available',
+        type: ''
+    }
+]
+const Monday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'G2',
+        subject: 'OCE',
+        type: 'Lecture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'G2',
+        subject: 'OCE',
+        type: 'Tutorial'
+    },
+    {   
+        time: '11.15-01.15 PM',
+        roomNumber: 'G5',
+        subject: 'IS',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-04 PM',
+        roomNumber: 'Lab 4',
+        subject: 'WT',
+        type: 'Practical'
+    },
+]
+const Tuesday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'G5',
+        subject: 'WT',
+        type: 'Leacture'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'G5',
+        subject: 'PE2',
+        type: 'Lecture'
+    },
+    {   
+        time: '11.15-1 PM',
+        roomNumber: 'G2',
+        subject: 'OCE',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-04 PM',
+        roomNumber: 'Lab 4',
+        subject: 'WT',
+        type: 'Practical'
+    },
+]
+
+const Wednesday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: '33-309',
+        subject: 'IS',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'G5',
+        subject: 'WT',
+        type: 'Lecture'
+    }
+]
+
+const Thursday =[
+    {   
+        time: '11-12 AM',
+        roomNumber: 'G5',
+        subject: 'CD',
+        type: 'Lecture'
+    },
+    {   
+        time: '01-02 PM',
+        roomNumber: 'G5',
+        subject: 'IS',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'G5',
+        subject: 'PE2',
+        type: 'Lecture'
+    }
+]
+
+const Friday =[
+    {   
+        time: '10-11 AM',
+        roomNumber: 'G5',
+        subject: 'PE2',
+        type: 'Lecture'
+    },
+    {   
+        time: '11-12 AM',
+        roomNumber: 'G5',
+        subject: 'CD',
+        type: 'Lecture'
+    },
+    {   
+        time: '02-03 PM',
+        roomNumber: 'G5',
+        subject: 'IS',
+        type: 'Tutorial'
+    },
+
+]
+
+const Saturday =[
+    {   
+        time: '09-10 AM',
+        roomNumber: 'Lab No 1',
+        subject: 'IS',
+        type: 'Tutorial'
+    },
+    {   
+        time: '10-11 AM',
+        roomNumber: 'Lab No 1',
+        subject: 'IS',
+        type: 'Tutorial'
+    },
+
+]
